a9e9c84ee7fc662f9bcfe8ebeada2f139e080d61
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2026-01-31 16:37:31 +0700

    Add: jwt login user

--- backend/src/controllers/client/auth.controller.js
+++ backend/src/controllers/client/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../../models/user.model.js";
+import jwt from "jsonwebtoken";
 
-// [post] /auth/login
+// [POST] /auth/login
 export const loginPost = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -24,24 +25,37 @@ export const loginPost = async (req, res) => {
       });
     }
 
-    // hash password.
-
-    if (!passwordMatch) {
+    if (password !== user.password) {
       return res.status(400).json({
         code: 400,
         message: "Mật khẩu không chính xác!",
       });
     }
 
-    // res.cookie("token", user.id, {
-    //   httpOnly: true,
-    //   expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    // });
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      process.env.ACCESS_TOKEN_SECRET || "secret-123",
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    });
 
     res.json({
       code: 200,
       message: "Đăng nhập thành công!",
-      user: user,
+      token: token,
+      user: {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+        avatar: user.avatar,
+      },
     });
   } catch (error) {
     console.error(error);
